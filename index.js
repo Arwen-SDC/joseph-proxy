575650633ac5d6186852faf49295a6bb47b7ad2b
Joseph765 <joseph.chauvin102@gmail.com>
2020-06-05 13:19:26 -0500

    Added extra modules and egt requests

--- index.js
+++ index.js
@@ -10,21 +10,36 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 app.use(express.json());
 app.use('/games/:id', express.static(`${__dirname}/public`));
 
-app.get('/cartapi/:id', (req, res) => {
+app.get('/cartapi/:gameId', (req, res) => {
   axios({
-    baseURL: `http://ec2-13-59-39-155.us-east-2.compute.amazonaws.com:3001/cartapi/${req.params.id}`
+    baseURL: `http://ec2-13-59-39-155.us-east-2.compute.amazonaws.com:3001/cartapi/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
+});
+
+app.get('/api/games/:gameId', (req, res) => {
+  axios({
+    baseURL: `http://54.67.69.54:3004/api/games/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
+});
+
+app.get('/reviews/:gameId', (req, res) => {
+  axios({
+
+    baseURL: `http://18.220.38.54:3002/reviews/${req.params.gameId}`
   })
     .then((data) => { res.status(200).send(data.data); })
     .catch(() => { res.status(404).end(); });
 });
 
 app.post('/addGame/:id', (req, res) => {
-  res.send(200);
+  const newGame = {'csgo': "silver4"};
+  res.send(newGame);
+
 });
-
-// app.use('/cartapi/:id', createProxyMiddleware({target: 'localhost:3001', changeOrigin: true}));
-// app.use('/addGame/:id', createProxyMiddleware({target: 'localhost:3001', changeOrigin: true}));
-
 
 app.listen(PORT, () => {
   console.log(`Listening at port:${PORT}`);
